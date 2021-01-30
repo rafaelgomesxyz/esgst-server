7406800a6cd151d8e669c0a1de551d160854b524
rafaelgomesxyz <rafaelgomesxyz@gmail.com>
2021-01-30 12:14:22 -0300

    Add UST route

--- app/esgst/routes/index.js
+++ app/esgst/routes/index.js
@@ -5,6 +5,7 @@ const SgIds = require('./games/SgIds');
 const Rcv = require('./games/Rcv');
 const Ncv = require('./games/Ncv');
 const Uh = require('./users/Uh');
+const Ust = require('./users/Ust');
 const SettingsStats = require('./settings/Stats');
 
 const routes = Router();
@@ -17,6 +18,7 @@ routes.get('/esgst/games/ncv', Ncv.get);
 routes.post('/esgst/games/ncv', Ncv.post);
 routes.get('/esgst/user/\\+:steamid/uh', Uh.get);
 routes.get('/esgst/users/uh', Uh.get);
+routes.get('/esgst/users/ust', Ust.get);
 routes.get('/esgst/settings/stats', SettingsStats.get);
 routes.post('/esgst/settings/stats', SettingsStats.post);
 
