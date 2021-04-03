54a6c90ed110b2852a5a18167eb61173b557d422
rafaelgomesxyz <rafaelgomesxyz@gmail.com>
2021-04-03 10:46:56 -0300

    Add rate limit

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const routes = require('./app/esgst/routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const limiter = rateLimit({ max: 30 });
+
 app.enable('trust proxy');
+app.use(limiter);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
